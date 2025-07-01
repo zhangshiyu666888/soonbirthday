6eef48231e63e0900b5acfb13f3213970b0fd582
zhangshiyu666888 <2410421@tongji.edu.cn>
2025-07-01 16:41:06 +0800

    Update indexl.js

--- js/indexl.js
+++ js/indexl.js
@@ -32,7 +32,7 @@ var S = {
             m++;
       S.Shape.render();
       //console.log(m);
-      if(m==700){
+      if(m==2800){
         window.location.href="../html/BirthdayCake.html";
       }
     });
